20b18ab428992769ed7d15cdaf587f3c44f4d2a3
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-29 17:01:53 +0100

    Start of http module

--- app.js
+++ app.js
@@ -1,42 +1,9 @@
-//const {readFileSync, writeFileSync} = require('fs');
-const {readFile, writeFile} = require('fs');
+const http = require('http');
 
-/* Sync:
-const first = readFileSync('./content/first.txt', 'utf8');
-console.log(first);
-const second = readFileSync('./content/second.txt', 'utf8');
-console.log(second);
+const server = http.createServer((req, res) => {
+    res.write('Welcome to NodeJS');
+    res.end();
+})
 
-writeFileSync(
-    './content/result-sync.txt',
-    `Result - first: ${first} and second: ${second} together!`,
-    { flag: 'a'} // flag to append results instead of rewriting
-);*/
-
-// Async:
-readFile('./content/first.txt', 'utf8', (err, result1)=> {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(result1);
-    const first = result1;
-    readFile('./content/second.txt', 'utf8', (err, result2) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(result2);
-        const second = result2;
-
-        writeFile(
-            './content/result-async.txt',
-            `Here is the result: ${first} | ${second}`,
-            (err, final_result) => {
-                if (err) {
-                    return console.log(err);
-                }
-                console.log(final_result);
-            }
-        )
-    })
-});
+server.listen(5000);
 
