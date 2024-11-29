bb98d19db0eaca8183705f539de07d38aa570454
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-29 16:36:33 +0100

    FS module (async)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const {readFileSync, writeFileSync} = require('fs');
+//const {readFileSync, writeFileSync} = require('fs');
+const {readFile, writeFile} = require('fs');
 
+/* Sync:
 const first = readFileSync('./content/first.txt', 'utf8');
 console.log(first);
 const second = readFileSync('./content/second.txt', 'utf8');
@@ -9,4 +11,32 @@ writeFileSync(
     './content/result-sync.txt',
     `Result - first: ${first} and second: ${second} together!`,
     { flag: 'a'} // flag to append results instead of rewriting
-);
+);*/
+
+// Async:
+readFile('./content/first.txt', 'utf8', (err, result1)=> {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(result1);
+    const first = result1;
+    readFile('./content/second.txt', 'utf8', (err, result2) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(result2);
+        const second = result2;
+
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first} | ${second}`,
+            (err, final_result) => {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log(final_result);
+            }
+        )
+    })
+});
+
